c95bbe152fda349ecd4a327c119d4c08f5c7888e
gennarocc <gennaroccuomo@gmail.com>
2022-02-02 12:16:53 -0500

    nvim fixes

--- config/nvim/chadrc.lua
+++ config/nvim/chadrc.lua
@@ -1,7 +1,7 @@
 -- IMPORTANT NOTE : This is the user config, can be edited. Will be preserved if updated with internal updater
 -- This file is for NvChad options & tools, custom settings are split between here and 'lua/custom/init.lua'
 local M = {}
-local customPlugins = require "custom.plugins"
+local customPlugins = require("custom.plugins")
 
 M.options, M.ui, M.mappings, M.plugins = {}, {}, {}, {}
 
@@ -13,48 +13,48 @@ M.options, M.ui, M.mappings, M.plugins = {}, {}, {}, {}
 --    relativenumber = true,
 -- }
 M.mappings = {
-   window_nav = {
-      moveLeft = "<leader>wh",
-      moveRight = "<leader>wl",
-      moveUp = "<leader>wk",
-      moveDown = "<leadaer>wj",
-   },
-   terminal = {
-      new_window = "<C-w>",
-      new_horizontal = "<C-h>",
-      new_vertical = "<C-v>",
-   },
+  window_nav = {
+    moveLeft = "<leader>wh",
+    moveRight = "<leader>wl",
+    moveUp = "<leader>wk",
+    moveDown = "<leadaer>wj",
+  },
+  terminal = {
+    new_window = "<C-w>",
+    new_horizontal = "<C-h>",
+    new_vertical = "<C-v>",
+  },
 }
 
 M.ui = { theme = "gruvchad", transparency = true }
 
 -- NvChad included plugin options & overrides
 M.plugins = {
-   install = customPlugins,
-   status = {
-      truzen = true,
-      colorizer = true,
-      orgmode = true,
-      neoformat = true,
-      neoscroll = true,
-   },
-   options = {
-      lspconfig = {
-         --    path of file containing setups of different lsps (ex : "custom.plugins.lspconfig"), read the docs for more info
-         setup_lspconf = "custom.plugins.lspconfig",
-      },
-      statusline = { style = "block" },
-   },
-   -- To change the Packer `config` of a plugin that comes with NvChad,
-   -- add a table entry below matching the plugin github name
-   --              '-' -> '_', remove any '.lua', '.nvim' extensions
-   -- this string will be called in a `require`
-   --              use "(custom.configs).my_func()" to call a function
-   --              use "custom.blankline" to call a file
-   default_plugin_config_replace = {
-      nvim_treesitter = "custom.plugins.treesitter",
-      nvim_tree = "custom.plugins.nvimtree",
-   },
+  install = customPlugins,
+  status = {
+    truzen = true,
+    colorizer = true,
+    orgmode = true,
+    neoformat = true,
+    neoscroll = true,
+  },
+  options = {
+    lspconfig = {
+      --    path of file containing setups of different lsps (ex : "custom.plugins.lspconfig"), read the docs for more info
+      setup_lspconf = "custom.plugins.lspconfig",
+    },
+    statusline = { style = "block" },
+  },
+  -- To change the Packer `config` of a plugin that comes with NvChad,
+  -- add a table entry below matching the plugin github name
+  --              '-' -> '_', remove any '.lua', '.nvim' extensions
+  -- this string will be called in a `require`
+  --              use "(custom.configs).my_func()" to call a function
+  --              use "custom.blankline" to call a file
+  default_plugin_config_replace = {
+    nvim_treesitter = "custom.plugins.treesitter",
+    nvim_tree = "custom.plugins.nvimtree",
+  },
 }
 
 return M
