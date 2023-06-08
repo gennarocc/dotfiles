6a77dcbdae6c7dca626e5a20c4018efbce927191
gennarocc <gennaroccuomo@gmail.com>
2023-06-08 11:24:36 -0400

    New zsh git functions

--- config/zsh/git-functions.zsh
+++ config/zsh/git-functions.zsh
@@ -1,12 +1,23 @@
-gb() {
- git checkout “$(git branch — all | fzf| tr -d ‘[:space:]’)”
+is_in_git_repo() {
+  git rev-parse HEAD > /dev/null 2>&1
 }
 
-glog() {
-  git log --graph --color=always \
-      --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
-  fzf --ansi --no-sort --reverse --tiebreak=index --toggle-sort=\` \
-      --bind "ctrl-m:execute:
-                echo '{}' | grep -o '[a-f0-9]\{7\}' | head -1 |
-                xargs -I % sh -c 'git show --color=always % | less -R'"
+gf() {
+  is_in_git_repo &&
+    git -c color.status=always status --short |
+    fzf --height 40% -m --ansi --nth 2..,.. | awk '{print $2}'
 }
+
+gb() {
+  is_in_git_repo &&
+    git branch -a -vv --color=always | grep -v '/HEAD\s' |
+    fzf --height 40% --ansi --multi --tac | sed 's/^..//' | awk '{print $1}' |
+    sed 's#^remotes/[^/]*/##'
+}
+
+gh() {
+  is_in_git_repo &&
+    git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph |
+    fzf --height 40% --ansi --no-sort --reverse --multi | grep -o '[a-f0-9]\{7,\}'
+}
+
