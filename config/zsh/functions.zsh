596152c3334c769afb1d1fcd83d556f069c17f65
Gennaro Cuomo <gennarocc@proton.me>
2024-05-02 11:23:24 -0400

    Update functions.zsh

--- config/zsh/functions.zsh
+++ config/zsh/functions.zsh
@@ -28,38 +28,6 @@ function fw () {
 #   cd $projects/$project
 # }
 
-# Git Function
-
-function fb(){
-    if git rev-parse --git-dir > /dev/null 2>&1; then
-        if [[ "$#" -eq 0 ]]; then
-            local branches branch
-            branches=$(git branch -a) &&
-            branch=$(echo "$branches" |
-            fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
-            git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
-        elif [ `git rev-parse --verify --quiet $*` ] || \
-             [ `git branch --remotes | grep  --extended-regexp "^[[:space:]]+origin/${*}$"` ]; then
-            echo "Checking out to existing branch"
-            git checkout "$*"
-        else
-            echo "Creating new branch"
-            git checkout -b "$*"
-        fi
-    else
-        echo "Can't check out or create branch. Not in a git repo"
-    fi
-}
-
-function glog () {
-  git log --graph --color=always \
-      --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
-  fzf --ansi --no-sort --reverse --tiebreak=index --toggle-sort=\` \
-      --bind "ctrl-m:execute:
-                echo '{}' | grep -o '[a-f0-9]\{7\}' | head -1 |
-                xargs -I % sh -c 'git show --color=always % | less -R'"
-}
-
 # DOCKER FUNCTIONS
 
 function fdrc () {
