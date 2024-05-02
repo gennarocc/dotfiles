61de7680613775fb27c8b296db0d03cfb3ff80c6
Gennaro Cuomo <gennarocc@proton.me>
2024-05-02 11:26:06 -0400

    Update functions.zsh

--- config/zsh/functions.zsh
+++ config/zsh/functions.zsh
@@ -13,21 +13,6 @@ function fw () {
   cd $projects/$project
 }
 
-# function fc () {
-#   local projects=$HOME/.dotfiles/config
-#
-#   if [[ ! -d $projects ]]; then
-#     echo -e "$projects dir does not exist"
-#     return 1
-#   fi
-#
-#   local project=$(fd -t d --max-depth 1 . $projects | awk -F/ '{print $(NF-1)}' | fzf)
-#
-#   [[ -z $project ]] && return
-#
-#   cd $projects/$project
-# }
-
 # DOCKER FUNCTIONS
 
 function fdrc () {
@@ -46,8 +31,6 @@ function fdri () {
   docker rmi $(echo $images) -f
 }
 
-
 alias fdrc="fdrc"
 alias fdri="fdri"
 alias fw="fw"
-alias fc="fc"
