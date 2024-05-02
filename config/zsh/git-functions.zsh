5b373f02bf09ffdd8f5d2ea5758984b8c09a0881
Gennaro Cuomo <gennarocc@proton.me>
2024-05-02 11:25:32 -0400

    Update git-functions.zsh

--- config/zsh/git-functions.zsh
+++ config/zsh/git-functions.zsh
@@ -1,28 +1,5 @@
 # Git Function
 
-is_in_git_repo() {
-  git rev-parse HEAD > /dev/null 2>&1
-}
-
-gf() {
-  is_in_git_repo &&
-    git -c color.status=always status --short |
-    fzf --height 40% -m --ansi --nth 2..,.. | awk '{print $2}'
-}
-
-gb() {
-  is_in_git_repo &&
-    git branch -a -vv --color=always | grep -v '/HEAD\s' |
-    fzf --height 40% --ansi --multi --tac | sed 's/^..//' | awk '{print $1}' |
-    sed 's#^remotes/[^/]*/##'
-}
-
-gh() {
-  is_in_git_repo &&
-    git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph |
-    fzf --height 40% --ansi --no-sort --reverse --multi | grep -o '[a-f0-9]\{7,\}'
-}
-
 function fb(){
     if git rev-parse --git-dir > /dev/null 2>&1; then
         if [[ "$#" -eq 0 ]]; then
@@ -52,3 +29,6 @@ function glog () {
                 echo '{}' | grep -o '[a-f0-9]\{7\}' | head -1 |
                 xargs -I % sh -c 'git show --color=always % | less -R'"
 }
+
+alias gb="gb"
+alias glog="glog"
